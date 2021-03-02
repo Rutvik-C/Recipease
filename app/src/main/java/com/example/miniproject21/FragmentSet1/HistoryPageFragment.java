0cd08b591c1def7fa218b3f9b38418d67237b57c
malharbangdiwala <malharbangdiwala03@gmail.com>
2021-03-02 14:29:58 +0530

    Added edit profile page

--- app/src/main/java/com/example/miniproject21/FragmentSet1/HistoryPageFragment.java
+++ app/src/main/java/com/example/miniproject21/FragmentSet1/HistoryPageFragment.java
@@ -1,21 +1,48 @@
 package com.example.miniproject21.FragmentSet1;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.miniproject21.EditProfile;
 import com.example.miniproject21.R;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class HistoryPageFragment extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_history_page, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final Button editProfileButton=requireView().findViewById(R.id.editProfileButton);
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), EditProfile.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
