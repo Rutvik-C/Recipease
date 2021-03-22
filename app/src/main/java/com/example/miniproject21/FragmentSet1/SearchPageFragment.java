9e661c2494b7306b421dfeff3e7f4babd88cef82
yashvimehta <yashvimehta45@gmail.com>
2021-03-22 17:36:44 +0530

    changes in search

--- app/src/main/java/com/example/miniproject21/FragmentSet1/SearchPageFragment.java
+++ app/src/main/java/com/example/miniproject21/FragmentSet1/SearchPageFragment.java
@@ -65,31 +65,20 @@ import java.util.Map;
 import static android.R.layout.simple_list_item_1;
 
 public class SearchPageFragment extends Fragment {
-    //FirebaseAuth firebaseAuth;
-    //FirebaseUser user = firebaseAuth.getCurrentUser();
-    // public static RecyclerView toptenRecyclerView;
     ArrayList<TopTenModel> topTenArrayList;
     ArrayList<String> topNameArrayList;
     ListView topTenListView;
     CustomCardAdapter mCustomCardAdapter;
 
-    ArrayList<String> keys;
-    HashMap<String, Integer> foodAndCount = new HashMap<String, Integer>();
-    Map<String, Integer> foodAndCount1;
-    String[] foodArray;
 
-    AutoCompleteTextView autoCompleteTextView;
+    //AutoCompleteTextView autoCompleteTextView;
 
-    ArrayAdapter<String> arrayAdapter1;
     TextView text;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_search_page, container, false);
-        autoCompleteTextView = view.findViewById(R.id.autoCompleteTextView);
 
-        //toptenRecyclerView = getActivity().findViewById(R.id.idtopten);
-        // here we have created new array list and added data to it.
         text = (TextView) view.findViewById(R.id.textSearch);
 
         // Inflate the layout for this fragment
@@ -136,70 +125,16 @@ public class SearchPageFragment extends Fragment {
             }
         });
 
-
-//        FirebaseFirestore db = FirebaseFirestore.getInstance();
-//        final String[] s = new String[10];
+//        autoCompleteTextView.setOnTouchListener(autoOnTouch);
+//        //autoCompleteTextView.setOnKeyListener(autoOnKey);
+//        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //
-//        db.collection("foodItems")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document:task.getResult()){
-//                                if(document.getData().get("count")!=null)
-//                                foodAndCount.put(document.getId(),Integer.parseInt(document.getData().get("count").toString()));
-//                            }
-//                            foodAndCount1 = sortByValue(foodAndCount);
-//                            keys = new ArrayList<String>(foodAndCount1.keySet());
-//                            //for(int i=keys.size()-1;i>=keys.size()-10;i--){
-//                            // toptenArrayList.add(new TopTenModel(keys.get(i), foodAndCount1,R.drawable.rose));
-//                            //s[keys.size()-i-1]=keys.get(i);
-//                            //}
-//                            int i=1;
-//                            for (Map.Entry<String, Integer> set : foodAndCount1.entrySet()) {
-//                                toptenArrayList.add(new TopTenModel(set.getKey(), String.valueOf(set.getValue()),R.drawable.rose));
-//                                i++;
-//                                if(i>10)
-//                                    break;
-//                            }
-//                            // we are initializing our adapter class and passing our arraylist to it.
-//                            TopTenCardAdapter toptenAdapter = new TopTenCardAdapter(requireContext(), toptenArrayList);
+//            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+//                Log.i("selected", arg0.getItemAtPosition(arg2).toString());
+//                Toast.makeText(view.getContext(), arg0.getItemAtPosition(arg2).toString() + " details will be displayed ", Toast.LENGTH_SHORT).show();
 //
-//                            // below line is for setting a layout manager for our recycler view.
-//                            // here we are creating vertical list so we will provide orientation as vertical
-//                            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
-//
-//                            // in below two lines we are setting layoutmanager and adapter to our recycler view.
-//                            toptenRecyclerView.setLayoutManager(linearLayoutManager);
-//                            toptenRecyclerView.setAdapter(toptenAdapter);
-//                            Collections.sort(keys);
-//                        } else {
-//                            Log.i("error", "Error getting documents.", task.getException());
-//                        }
-//                        foodArray = new String[keys.size()];
-//                        foodArray = keys.toArray(foodArray);
-//                        arrayAdapter1 = new ArrayAdapter<String>(view.getContext(),simple_list_item_1,foodArray);
-//                        autoCompleteTextView.setAdapter(arrayAdapter1);
-//                        autoCompleteTextView.setThreshold(1);
-//
-//
-//                    }
-//                });
-
-
-        autoCompleteTextView.setOnTouchListener(autoOnTouch);
-        //autoCompleteTextView.setOnKeyListener(autoOnKey);
-        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                Log.i("selected",arg0.getItemAtPosition(arg2).toString());
-                Toast.makeText(view.getContext(), arg0.getItemAtPosition(arg2).toString()+" details will be displayed ",  Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-
+//            }
+//        });
 
 
     }
@@ -215,42 +150,8 @@ public class SearchPageFragment extends Fragment {
         }
     };
 
-//    private static View.OnKeyListener autoOnKey = new View.OnKeyListener() {
-//        public boolean onKey(View v, int keyCode, KeyEvent event) {
-//            if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
-//                //your code
-//                return true;
-//            } else {
-//                return false;
-//            }
-//        }
-//    };
-
-
-    public void hideCardAndText(){
+    public void hideCardAndText() {
         text.setVisibility(View.INVISIBLE);
         //toptenRecyclerView.setVisibility(View.INVISIBLE);
-    }
-    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm)
-    {
-        // Create a list from elements of HashMap
-        List<Map.Entry<String, Integer> > list =
-                new LinkedList<Map.Entry<String, Integer> >(hm.entrySet());
-
-        // Sort the list
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
-            public int compare(Map.Entry<String, Integer> o1,
-                               Map.Entry<String, Integer> o2)
-            {
-                return (o1.getValue()).compareTo(o2.getValue());
-            }
-        });
-
-        // put data from sorted list to hashmap
-        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
-        for (Map.Entry<String, Integer> aa : list) {
-            temp.put(aa.getKey(), aa.getValue());
-        }
-        return temp;
     }
 }
