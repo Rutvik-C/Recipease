12082c5ed8d2d9c4f25f9adf6bea4bf4a2f0701b
yashvimehta <yashvimehta45@gmail.com>
2021-03-26 22:30:45 +0530

    Recipe fragment

--- app/src/main/java/com/example/miniproject21/FragmentResult/RecipeFragment.java
+++ app/src/main/java/com/example/miniproject21/FragmentResult/RecipeFragment.java
@@ -1,6 +1,7 @@
 package com.example.miniproject21.FragmentResult;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,13 @@ import com.example.miniproject21.R;
 import com.example.miniproject21.RecipeCard.RecipeCardAdapter;
 import com.example.miniproject21.RecipeCard.RecipeCardModel;
 import com.example.miniproject21.ResultsActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
@@ -23,6 +31,11 @@ public class RecipeFragment extends Fragment {
 
     public static RecyclerView recipeRecyclerView;
     public ArrayList<RecipeCardModel> recipeArrayList;
+
+    ArrayList<String> ingredients;
+    ArrayList<String> steps;
+    FirebaseFirestore db;
+    FirebaseUser mUser;
 
 
     @Override
@@ -40,6 +53,8 @@ public class RecipeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_recipe_result, container, false);
 
         // code here
+        db = FirebaseFirestore.getInstance();
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
 
         return view;
     }
@@ -48,25 +63,51 @@ public class RecipeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         recipeRecyclerView = getActivity().findViewById(R.id.idRecipe);
+        ingredients = new ArrayList<String>();
+        steps = new ArrayList<String>();
+
         // here we have created new array list and added data to it.
-        recipeArrayList = new ArrayList<>();
-        recipeArrayList.add(new RecipeCardModel("DSA in Java", "4"));
-        recipeArrayList.add(new RecipeCardModel("Java Course", "1"));
-        recipeArrayList.add(new RecipeCardModel("C++ Course", "4"));
-        recipeArrayList.add(new RecipeCardModel("DSA in C++", "1"));
-        recipeArrayList.add(new RecipeCardModel("Kotlin for Android", "!"));
-        recipeArrayList.add(new RecipeCardModel("Java for Android", "Abc"));
-        recipeArrayList.add(new RecipeCardModel("HTML and CSS", "abc"));
 
-        // we are initializing our adapter class and passing our arraylist to it.
-        RecipeCardAdapter recipeAdapter = new RecipeCardAdapter(requireContext(), recipeArrayList);
+        DocumentReference docRef = db.collection("foodItems").document(ResultsActivity.item);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        recipeArrayList = new ArrayList<>();
+                        recipeArrayList.add(new RecipeCardModel("Ingredients", ""));
+                        ingredients=(ArrayList<String>)document.getData().get("ingredients_recipe");
+                        for(int i=0;i<ingredients.size();i++){
+                            recipeArrayList.add(new RecipeCardModel("Ingredient no. "+String.valueOf(i+1), ingredients.get(i)));
+                        }
+                        recipeArrayList.add(new RecipeCardModel("Preparation Time",document.getData().get("r_prep_time").toString()));
+                        recipeArrayList.add(new RecipeCardModel("Cooking Time",document.getData().get("r_cook_time").toString()));
+                        recipeArrayList.add(new RecipeCardModel("Total Time",document.getData().get("r_total_time").toString()));
+                        recipeArrayList.add(new RecipeCardModel("Steps", ""));
+                        steps=(ArrayList<String>)document.getData().get("r_steps");
+                        for(int i=0;i<steps.size();i++){
+                            recipeArrayList.add(new RecipeCardModel("Step no. "+String.valueOf(i+1), steps.get(i)));
+                        }
+                        // we are initializing our adapter class and passing our arraylist to it.
+                        RecipeCardAdapter recipeAdapter = new RecipeCardAdapter(requireContext(), recipeArrayList);
 
-        // below line is for setting a layout manager for our recycler view.
-        // here we are creating vertical list so we will provide orientation as vertical
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
+                        // below line is for setting a layout manager for our recycler view.
+                        // here we are creating vertical list so we will provide orientation as vertical
+                        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
 
-        // in below two lines we are setting layoutmanager and adapter to our recycler view.
-        recipeRecyclerView.setLayoutManager(linearLayoutManager);
-        recipeRecyclerView.setAdapter(recipeAdapter);
+                        // in below two lines we are setting layoutmanager and adapter to our recycler view.
+                        recipeRecyclerView.setLayoutManager(linearLayoutManager);
+                        recipeRecyclerView.setAdapter(recipeAdapter);
+
+                    } else {
+                        Log.i("ttt", "No such document");
+                    }
+                } else {
+                    Log.i("ppp", "get failed with ", task.getException());
+                }
+            }
+        });
+
     }
 }
