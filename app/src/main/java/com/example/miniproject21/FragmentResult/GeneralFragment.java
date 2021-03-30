c87a870fe31b477ebb1bb54061e7577584ef445d
yashvimehta <yashvimehta45@gmail.com>
2021-03-30 22:19:24 +0530

    like button general fragment

--- app/src/main/java/com/example/miniproject21/FragmentResult/GeneralFragment.java
+++ app/src/main/java/com/example/miniproject21/FragmentResult/GeneralFragment.java
@@ -1,9 +1,11 @@
 package com.example.miniproject21.FragmentResult;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +16,10 @@ import com.example.miniproject21.R;
 import com.example.miniproject21.ResultsActivity;
 
 public class GeneralFragment extends Fragment {
+
+    ImageView imageView;
+
+    Boolean isLike=false;
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
@@ -33,6 +39,22 @@ public class GeneralFragment extends Fragment {
         TextView mTextView = view.findViewById(R.id.dishName);
         mTextView.setText(ResultsActivity.item);
 
+        imageView=view.findViewById(R.id.imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isLike){
+                    isLike=false;
+                    imageView.setImageResource(R.drawable.ic_like);
+                }else{
+                    isLike=true;
+                    imageView.setImageResource(R.drawable.ic_liked);
+                }
+            }
+        });
+
         return view;
     }
+
 }
