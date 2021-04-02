56f2ccd095770d86f7a019c65839ad883a8a65f2
Rutvik-C <rutvikchoudhari97@gmail.com>
2021-04-02 11:48:20 +0530

    Images loading on LVs

--- app/src/main/java/com/example/miniproject21/CustomCardAdapter.java
+++ app/src/main/java/com/example/miniproject21/CustomCardAdapter.java
@@ -2,6 +2,7 @@ package com.example.miniproject21;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewpager.widget.ViewPager;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.miniproject21.TopTenCard.TopTenModel;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -39,7 +45,7 @@ public class CustomCardAdapter extends ArrayAdapter<String> {
 
         TextView mTextView1 = view.findViewById(R.id.dishNameTextView);
         TextView mTextView2 = view.findViewById(R.id.dishCountTextView);
-        ImageView mImageView = view.findViewById(R.id.dishImageView);
+        final ImageView mImageView = view.findViewById(R.id.dishImageView);
 
         mTextView1.setText(mArrayList.get(position).getDishname());
 
@@ -47,7 +53,18 @@ public class CustomCardAdapter extends ArrayAdapter<String> {
             mTextView2.setText(mArrayList.get(position).getDishcount());
         }
 
-        mImageView.setImageResource(mArrayList.get(position).getDishimage());
+        StorageReference mRef = FirebaseStorage.getInstance().getReference("icons").child(mArrayList.get(position).getDishname() + ".png");
+        mRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(getContext())
+                        .load(uri)
+                        .placeholder(R.drawable.image_progress)
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .into(mImageView);
+
+            }
+        });
 
         return view;
     }
